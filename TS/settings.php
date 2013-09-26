3c33afd25a4b6b55bd0722098bf6f3168b2414d8
Dmitry Chabanenko <chdn6026@mail.ru>
2013-09-26 14:27:03 +0300

    Changed settings

--- TS/settings.php
+++ TS/settings.php
@@ -10,12 +10,6 @@ $uploaddirroot = '/home/localhost/www/uploads/';// папка для завантаження резуль
 }
 else
 {
-define("DBName","chabane_TS");
-define("DBName2","chabane_prognoz2");
-define("HostName","localhost");
-define("UserName","chabane_site2");
-define("Password","prognoz");
-$uploaddirroot = '/hsphere/local/home/chabanenko/prognoz.ck.ua/uploads/';
 }
 //echo DBName;
 
